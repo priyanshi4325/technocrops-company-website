262f329d65e9a2fbde87c2abcc469ed6af14c53b
priyanshi4325 <priyanshdass@gmail.com>
2026-03-08 15:33:21 +0530

    Changed email port

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = 'django-insecure-wmb56+j=sf_uovma%trc44g86$54cd&sq-ix1k22o-a51v)3wd
 DEBUG = False
 ALLOWED_HOSTS = ['*']
 
-EMAIL_TIMEOUT = 10
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -129,11 +129,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
+EMAIL_TIMEOUT = 5
 
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
